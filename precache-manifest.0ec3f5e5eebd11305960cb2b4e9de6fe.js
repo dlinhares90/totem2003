681b4aa6cf8d93504213db944cb8704dc7ae86c6
Douglas <douglas.linhares@hammerconsult.com.br>
2023-03-20 18:17:15 -0300

    n

--- precache-manifest.0ec3f5e5eebd11305960cb2b4e9de6fe.js
+++ precache-manifest.0ec3f5e5eebd11305960cb2b4e9de6fe.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83699c25b55591ee69af0c7456dad5dd",
+    "revision": "2f006db3dc6afbaaf1adb294a978c962",
     "url": "/index.html"
   },
   {
-    "revision": "cd96d4450cbd3f7955dd",
+    "revision": "40f4d6a696cb2a706ac3",
     "url": "/static/css/main.03d9584e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9f522b32.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cd96d4450cbd3f7955dd",
-    "url": "/static/js/main.078afd8b.chunk.js"
+    "revision": "40f4d6a696cb2a706ac3",
+    "url": "/static/js/main.30ce06c0.chunk.js"
   },
   {
     "revision": "7856968030923daf9d70",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4e30212d54cc4c53e709051e0227ed7.js"
+  "/precache-manifest.0ec3f5e5eebd11305960cb2b4e9de6fe.js"
 );
 
 self.addEventListener('message', (event) => {
